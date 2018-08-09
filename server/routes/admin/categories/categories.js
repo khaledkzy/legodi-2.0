fc0321b4fa9da6e4a05c744ccf513222e531adb0
khaledkzy <khaledkzy@gmail.com>
2018-08-09 07:52:19 +0100

    fix: add admin files and api

--- server/routes/admin/categories/categories.js
+++ server/routes/admin/categories/categories.js
@@ -1,87 +1,99 @@
-// var express = require('express')
-// var router = express.Router()
-// const categoryClient = require('../../../dbClients/categoriesDB')
+var express = require('express')
+var router = express.Router()
+const categoryClient = require('../../../dbClients/categoriesDB')
 // const articleClient = require('../../../dbClients/articlesDB')
 // const ObjectId = require('mongodb').ObjectID
 
-// router.get('/', (req, res, next) => {
-//   const callback = (error, category) => {
-//     res.render('admin-list-categories', {
-//       category,
-//       categoryHome: 'homeNav'
-//     })
-//   }
-//   categoryClient.findCategories({}, callback)
-// })
+router.get('/', (req, res, next) => {
+  const callback = (error, category) => {
+    if (error) {
+      next(error) // Pass errors to Express.
+    } else {
+      res.render('admin-list-categories', {
+        category,
+        categoryHome: 'homeNav'
+      })
+    }
+  }
+  categoryClient.findCategories({}, callback)
+})
 
-// router.get('/add', (req, res, next) => {
-//   res.render('admin-add-category', {
-//     addcategoryHome: 'homeNav'
-//   })
-// })
+router.get('/add', (req, res, next) => {
+  res.render('admin-add-category', {
+    addcategoryHome: 'homeNav'
+  })
+})
 
-// router.post('/add', (req, res, next) => {
-//   const query = req.body
-//   const titleTranslation = {}
+router.post('/add', (req, res, next) => {
+  const query = req.body
+  const titleTranslation = {}
 
-//   titleTranslation.am = query.am
-//   titleTranslation.ar = query.ar
+  titleTranslation.am = query.am
+  titleTranslation.ar = query.ar
 
-//   query.titleTranslation = titleTranslation
+  query.titleTranslation = titleTranslation
 
-//   if (query.icon == '') {
-//     query.icon = 'default-icon'
-//   }
-//   const callback = () => {
-//     res.redirect('/admin/categories')
-//   }
-//   categoryClient.addCategory(query, callback)
-// })
+  if (query.icon === '') {
+    query.icon = 'default-icon'
+  }
+  const callback = () => {
+    res.redirect('/admin/categories')
+  }
+  categoryClient.addCategory(query, callback)
+})
 
-// router.get('/edit/:categoryId', (req, res) => {
-//   const { categoryId } = req.params
-//   const callback = (error, category) => {
-//     res.render('admin-edit-category', {
-//       category: category,
-//       shortDescriptionTitle: 'Edit short',
-//       descriptionTitle: 'Edit'
-//     })
-//   }
-//   categoryClient.findCategoryById(categoryId, callback)
-// })
+router.get('/edit/:categoryId', (req, res, next) => {
+  const { categoryId } = req.params
+  const callback = (error, category) => {
+    if (error) {
+      next(error) // Pass errors to Express.
+    } else {
+      res.render('admin-edit-category', {
+        category: category,
+        shortDescriptionTitle: 'Edit short',
+        descriptionTitle: 'Edit'
+      })
+    }
+  }
+  categoryClient.findCategoryById(categoryId, callback)
+})
 
-// router.post('/delete/:categoryId', (req, res) => {
-//   const { categoryId } = req.params
+router.post('/delete/:categoryId', (req, res, next) => {
+  const { categoryId } = req.params
 
-//   callBack = (error, data) => {
-//     if (data.title === req.body.validationTitle) {
-//       deleteCallBack = () => {
-//         res.redirect('/admin/categories')
-//       }
-//       categoryClient.removeCategory(categoryId, deleteCallBack)
-//     } else {
-//       res.render('delete-title-wrong')
-//     }
-//   }
-//   categoryClient.findCategoryById(categoryId, callBack)
-// })
+  let callBack = (error, data) => {
+    if (data.title === req.body.validationTitle) {
+      if (error) {
+        next(error) // Pass errors to Express.
+      } else {
+        const deleteCallBack = () => {
+          res.redirect('/admin/categories')
+        }
+        categoryClient.removeCategory(categoryId, deleteCallBack)
+      }
+    } else {
+      res.render('delete-title-wrong')
+    }
+  }
+  categoryClient.findCategoryById(categoryId, callBack)
+})
 
-// router.post('/edit/:categoryId', (req, res, next) => {
-//   const { categoryId } = req.params
-//   const query = req.body
+router.post('/edit/:categoryId', (req, res, next) => {
+  const { categoryId } = req.params
+  const query = req.body
 
-//   const titleTranslation = {}
+  const titleTranslation = {}
 
-//   titleTranslation.am = query.am
-//   titleTranslation.ar = query.ar
+  titleTranslation.am = query.am
+  titleTranslation.ar = query.ar
 
-//   query.titleTranslation = titleTranslation
+  query.titleTranslation = titleTranslation
 
-//   const callback = (error, category) => {
-//     res.redirect('/admin/categories')
-//   }
+  const callback = (category) => {
+    res.redirect('/admin/categories')
+  }
 
-//   categoryClient.editCategory(categoryId, query, true, callback)
-// })
+  categoryClient.editCategory(categoryId, query, true, callback)
+})
 
-// module.exports = router
+module.exports = router
